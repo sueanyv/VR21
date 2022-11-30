96317e037c0f84192c1a9c2ab0cc6ec7b7c90bd6
sueanyv <valenciasue@hotmail.com>
2022-11-30 08:54:15 -0800

    consolelog

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+console.log("Is this working?")
+const name = "Sugey";
+const lasname ="Valencia";
+const favcolor = "light blue";
+const discolor ="pink";
+const bolTrue = true;
+const bros = 3;
+const sis = 1;
+const ageSentence = "23"
+const bolFalse = false;
+var num = 12;
+
+
+console.log(`My name is ${name} ${lasname}`);
+console.log(`Lives in Seattle:`, bolTrue);
+console.log(`I have ${bros} brothers and ${sis} sister`);
+console.log(`I have ${bros + sis} siblings`);
+console.log( typeof bros);
+console.log(`My ${bros} brothers live in Washington:`, false);
+console.log(sis<bros);
+console.log( typeof bolTrue );
+console.log(sis  === favcolor);
+console.log(favcolor === "light blue");
+console.log( bros % sis );
+console.log(Number.parseInt(bros));
+console.log(Number.parseInt("45"));
+console.log(`I have ` + num.toString() + ` books` );  
+console.log( typeof num.toString() );
